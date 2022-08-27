b19b5294bb60b22fe6a55433f6b0a7d9771ea2be
Abel Acuña <ale_lasarte@hotmail.com>
2022-08-27 00:29:31 -0300

    Solucionado problemas de responsive

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,7 @@
 import { createTable } from "./create-table.js";
+import { hamburgerSpin } from "./hamburger.js";
+
+hamburgerSpin();
 
 /* DOCUMENT START */
 document.addEventListener("DOMContentLoaded", createTable);
